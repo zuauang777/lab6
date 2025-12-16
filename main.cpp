04781ab5c2af3e8d4561a28f05f6e22663cd74ee
kocicka17 <oliast2008@gmail.com>
2025-12-17 00:51:00 +0200

    Some Changes)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int A[12], neparni = 0, index = 0;
+    int A[12], neparni = 0, C[12], index = 0;
 
 
     // введення елементів мисиву
@@ -16,7 +16,7 @@ int main()
     // шукаємо 2 непарний елемент
     for (int i = 0; i < 12; i++)
     {
-        if (A[i] %2 != 0)
+        if (A[i] % 2 != 0)
         {
             neparni++;
 
@@ -26,7 +26,7 @@ int main()
             }
         }
     }
-if (neparni < 2)
+    if (neparni < 2)
     {
         cout << "Другого непарного елемента не існує";
         return 0;
@@ -42,7 +42,17 @@ if (neparni < 2)
         }
         else
         {
-            C[i] = A[i]/A[index];
+            C[i] = A[i] / A[index];
         }
     }
+
+
+
+    // вивід нового масиву
+    for (int i = 0; i < 12; i++)
+    {
+        cout << C[i] << " ";
+    }
+
+    return 0;
 }
