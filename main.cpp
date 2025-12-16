f56d3947f3413d8e3d3068ce17110dc0db78e47e
Bogia_Leg <bogialeg@gmail.com>
2025-12-16 22:22:25 +0200

    Добавив пошук 2 непарного елемента

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
-#include <iostream> 
-using namespace std; 
- 
-int main() 
-{ 
-    int A[12]; 
- 
- 
-    // введення елементів мисиву 
-    for (int i = 0; i < 12; i++) 
-    { 
-        cin >> A[i]; 
-    } 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A[12], neparni = 0, index = 0;
+
+
+    // введення елементів мисиву
+    for (int i = 0; i < 12; i++)
+    {
+        cin >> A[i];
+    }
+
+
+    // шукаємо 2 непарний елемент
+    for (int i = 0; i < 12; i++)
+    {
+        if (A[i] %2 != 0)
+        {
+            neparni++;
+
+            if (neparni == 2)
+            {
+                index = i;
+            }
+        }
+    }
 }
