aeb5f7c3c4d626e89b36509be552e841debde3e3
zuauang <zuauang@gmail.com>
2025-12-16 22:04:09 +0200

    Додаю  C++ програму

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std; 
+ 
+int main() 
+{ 
+    int A[12]; 
+ 
+ 
+    // введення елементів мисиву 
+    for (int i = 0; i < 12; i++) 
+    { 
+        cin >> A[i]; 
+    } 
+}
